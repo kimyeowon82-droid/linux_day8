9992b3d45aad479e4f55371cb9c6b1235ce2b7bf
kopo00 <kopo00@kopo.ac.kr>
2026-05-21 07:10:20 +0000

    .

--- check.sh
+++ check.sh
@@ -8,8 +8,8 @@ fi
 if [ -d "$1" ]; then
     echo "$1 은 디렉터리입니다."
 elif [ -f "$1" ]; then
-    echo "$1 은 파일입니다"
+    echo "$1 은 파일입니다."
 else
-    echo "$1 은 존재하지 않습니다"
+    echo "$1 은 존재하지 않습니다."
 fi
     
